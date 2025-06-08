70b2eeadebbd78f02b8dc4814188a83a5ce74b8e
ding0t <david.ingot@gmail.com>
2025-06-08 22:51:13 +0800

    fixing syntax

--- utils/bible_md.py
+++ utils/bible_md.py
@@ -8,16 +8,16 @@ def create_markdown_files(output_dir):
     os.makedirs(output_dir, exist_ok=True)
 
     cards = f"""\
-    <div class="grid cards" markdown>\n
-    -   :fontawesome-solid-book-bible: __bible__\n
-        ---\n
-        Content\n
-        [:octicons-arrow-right-24: Customization](#)\n
-    -   :material-scale-balance: __scales__\n
-        ---\n
-        Content\n
-        [:octicons-arrow-right-24: License](#)\n
-    </div>"""
+<div class="grid cards" markdown>\n
+-   :fontawesome-solid-book-bible: __bible__\n
+    ---\n
+    Content\n
+    [:octicons-arrow-right-24: Customization](#)\n
+-   :material-scale-balance: __scales__\n
+    ---\n
+    Content\n
+    [:octicons-arrow-right-24: License](#)\n
+</div>\n"""
 
     for idx, book in enumerate(books, start=1):
         number = f"{idx:02}"  # Zero-padded to 2 digits
@@ -25,33 +25,33 @@ def create_markdown_files(output_dir):
         filepath = os.path.join(output_dir, filename)
 
         content = f"""\
-    ---
-    title: {number} {book}
-    tags: 
-        - bible
-    ---\n
-    ## About\n
-    <div class="grid cards" markdown>\n
-    - :octicons-person-24: __Author__ author
-    - :material-calendar-edit: __Author Date__ date\n
-    </div>\n
-    ## Memory verses\n\n
-    use md-bba\n
-    ## Attitudes\n
-    What attitudes are we taught\n
-    {cards}\n
-    ## Key Topics\n
-    {cards}\n
-    ## Types\n
-    What types are seen as shadows of a truth.\n
-    {cards}\n
-    ## Prophecies\n
-    {cards}\n
-    ## References\n
-    - [search icons](https://squidfunk.github.io/mkdocs-material/reference/icons-emojis/)"""
+---
+title: {number} {book}
+tags: 
+    - bible
+---\n
+## About\n
+<div class="grid cards" markdown>\n
+- :octicons-person-24: __Author__ author
+- :material-calendar-edit: __Author Date__ date\n
+</div>\n
+## Memory verses\n\n
+use md-bba\n
+## Attitudes\n
+What attitudes are we taught\n
+{cards}
+## Key Topics\n
+{cards}
+## Types\n
+What types are seen as shadows of a truth.\n
+{cards}
+## Prophecies\n
+{cards}
+## References\n
+- [search icons](https://squidfunk.github.io/mkdocs-material/reference/icons-emojis/)\n"""
 
         with open(filepath, "w", encoding="utf-8") as f:
-            f.write(dedent(content))
+            f.write(content)
         print(f"Created {filepath}")
 
 if __name__ == "__main__":
